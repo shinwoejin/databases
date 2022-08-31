68594fffa2976c0f0b8fdf2e46c726119647883c
shinwoejin <shinwoejin@naver.com>
2022-08-31 14:02:56 +0900

    commit

---
+++ world.sql
@@ -0,0 +1,50 @@
+
+CREATE TABLE city_me
+(
+  ID          INT          NOT NULL COMMENT '아이디',
+  Name        VARCHAR(200) NULL     COMMENT '도시명',
+  CountryCode VARCHAR(200) NOT NULL COMMENT '국가코드',
+  District    VARCHAR(200) NULL    ,
+  Population  INT          NULL    ,
+  PRIMARY KEY (ID)
+) COMMENT '도시';
+
+CREATE TABLE country_me
+(
+  CountryCode    VARCHAR(200) NOT NULL COMMENT '국가코드',
+  Code           VARCHAR(200) NULL    ,
+  Name           VARCHAR(200) NULL    ,
+  Continent      INT          NULL    ,
+  Region         VARCHAR(200) NULL    ,
+  SurfaceArea    FLOAT(10,2)  NULL    ,
+  	IndepYear     INT          NULL    ,
+  Population     INT          NULL    ,
+  LifeExpectancy 	FLOAT(3, 1) NULL    ,
+  GNP            FLOAT(10,2)  NULL    ,
+  GNPOld         FLOAT(10,2)  NULL    ,
+  LocalName      VARCHAR(200) NULL    ,
+  GovernmentForm VARCHAR(200) NULL    ,
+  HeadOfState    VARCHAR(200) NULL    ,
+  Capital        INT          NULL    ,
+  Code2          VARCHAR(200) NULL    ,
+  PRIMARY KEY (CountryCode)
+) COMMENT '국가';
+
+CREATE TABLE countrylanguage_me
+(
+  Language    VARCHAR(200) NOT NULL COMMENT '언어명',
+  CountryCode VARCHAR(200) NOT NULL COMMENT '국가코드',
+  IsOfficial  INT          NULL    ,
+  Percentage  FLOAT(4,1)   NULL    ,
+  PRIMARY KEY (Language)
+) COMMENT '국가별 언어';
+
+ALTER TABLE city_me
+  ADD CONSTRAINT FK_country_me_TO_city_me
+    FOREIGN KEY (CountryCode)
+    REFERENCES country_me (CountryCode);
+
+ALTER TABLE countrylanguage_me
+  ADD CONSTRAINT FK_country_me_TO_countrylanguage_me
+    FOREIGN KEY (CountryCode)
+    REFERENCES country_me (CountryCode);
