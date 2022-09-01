75c2ab05bf9a40363bc451ab46a12fa75763116f
shinwoejin <shinwoejin@naver.com>
2022-09-01 13:55:19 +0900

    COMMIT

--- Group_By.sql
+++ Group_By.sql
@@ -4,3 +4,14 @@ FROM Products
 GROUP BY CategoryID
 ORDER BY CategoryID ASC
 ;
+
+-- Aliases
+-- select CustomerID, Country
+SELECT TB_10.*
+FROM(
+SELECT COUNT(CustomerID) cnt, Country
+FROM Customers
+GROUP BY Country
+) as TB_10
+ORDER BY TB_10.cnt DESC;
+-- TB_10 을 넣어 테이블 임시 이름 지정
---
+++ programmers_Level1/최댓값 구하기.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59415
+-- 코드를 입력하세요
+SELECT DATETIME
+FROM ANIMAL_INS
+ORDER BY DATETIME DESC
+LIMIT 1
+;
