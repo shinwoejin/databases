b1d418441796ddb4dcc2d8d0364875a196bb5f42
shinwoejin <shinwoejin@naver.com>
2022-09-01 17:59:57 +0900

    COMMIT

--- programmers_Level2/이름에 el이 들어가는 동물 찾기.sql
+++ programmers_Level2/이름에 el이 들어가는 동물 찾기.sql
@@ -1,1 +1,7 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/59047
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE NAME LIKE '%el%' AND ANIMAL_TYPE LIKE 'Dog'
+ORDER BY NAME
+;
