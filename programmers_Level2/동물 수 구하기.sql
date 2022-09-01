b16f85ad474ba475d45f95bd489fc06afdf5e516
shinwoejin <shinwoejin@naver.com>
2022-09-01 20:06:30 +0900

    COMMIT

---
+++ programmers_Level2/동물 수 구하기.sql
@@ -0,0 +1,4 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59406
+-- 코드를 입력하세요
+SELECT COUNT(*)
+FROM ANIMAL_INS
---
+++ programmers_Level2/중복 제거하기.sql
@@ -0,0 +1,4 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59408
+-- 코드를 입력하세요
+SELECT COUNT (DISTINCT NAME) AS count 
+FROM ANIMAL_INS;
